cf2908be76f16f1e3ba8998dd2c3ad95a68d0d18
eliwebpro <elieziopingo1989@gmail.com>
2025-03-08 13:30:29 -0700

    update mngdb

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); // Load .env variables
 const express = require('express');
-const mongodb = require('./data/database');
+const mongodb = require('./data/database/database');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 // Initialize MongoDB connection
 mongodb.initDb((err) => {
     if (err) {
-        console.error("❌ Erro ao conectar ao MongoDB:", err);
+        console.error("Error to connect to MongoDB:", err);
     } else {
         app.listen(port, () => {
-            console.log(`🚀 Servidor rodando na porta: ${port}`);
+            console.log(`Server runing on port: ${port}`);
         });
     }
 });
