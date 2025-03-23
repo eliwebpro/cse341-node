ac9ec07447b7e9615d14424ad25e8b74e7bc3ab6
eliwebpro <elieziopingo1989@gmail.com>
2025-03-22 23:43:03 -0600

    cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // <--- Adicionado aqui
 const mongodb = require('./data/database');
 
 const swaggerUi = require('swagger-ui-express');
@@ -7,10 +8,11 @@ const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
+app.use(cors()); // <--- Habilita CORS para todas as origens
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.json()); 
-app.use('/', require('./routes')); 
+app.use(express.json());
+app.use('/', require('./routes'));
 
 const port = process.env.PORT || 8080;
 
